b5d2bd472bbb97f21550fae54e8e1f22ac8201d1
Danyl Demchenko <danyldemchenko.m@gmail.com>
2026-08-17 17:51:11 +0300

    discount test

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const discountBig = 50;
+  const discountSmall = 20;
+  const totalDays = days * 40;
+
+  if (days >= 7) {
+    return totalDays - discountBig;
+  } else if (days >= 3) {
+    return totalDays - discountSmall;
+  }
+
+  return totalDays;
 }
 
 module.exports = calculateRentalCost;
